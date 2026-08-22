ccde925009e78a92e7213c4dbfd730ef18f27dfa
ThierryHFR <thierryh@vivaldi.net>
2026-08-22 21:30:21 +0200

    escl: complete GPL headers for progressive decoders

--- backend/escl/escl_png.c
+++ backend/escl/escl_png.c
@@ -2,6 +2,22 @@
 
    Copyright (C) 2026 Thierry HUCHARD
 
+   This file is part of the SANE package.
+
+   SANE is free software; you can redistribute it and/or modify it under
+   the terms of the GNU General Public License as published by the
+   Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   SANE is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with sane; see the file COPYING.  If not, see
+   <https://www.gnu.org/licenses/>.
+
    This file implements progressive PNG streaming for the eSCL backend.  */
 
 #define DEBUG_DECLARE_ONLY
