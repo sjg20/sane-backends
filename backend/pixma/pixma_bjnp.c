2d07e1acd694c87ba2782191e0d7d668c02cbd5b
ThierryHFR <thierryh@vivaldi.net>
2026-08-24 19:08:54 +0200

    pixma: clean up BJNP device lifecycle

--- backend/pixma/pixma_bjnp.c
+++ backend/pixma/pixma_bjnp.c
@@ -1734,6 +1734,20 @@ static void bjnp_close_tcp(int devno)
   device[devno].open = 0;
 }
 
+static void
+bjnp_cleanup_devices (void)
+{
+  int i;
+
+  for (i = 0; i < bjnp_no_devices; i++)
+    {
+      bjnp_close_tcp (i);
+      bjnp_free_device_structure (i);
+    }
+
+  bjnp_no_devices = 0;
+}
+
 static BJNP_Status
 bjnp_allocate_device (SANE_String_Const devname,
                       SANE_Int * dn, char *resulting_host)
@@ -1997,7 +2011,7 @@ extern void
 sanei_bjnp_init (void)
 {
   DBG_INIT();
-  bjnp_no_devices = 0;
+  bjnp_cleanup_devices ();
 }
 
 /**
@@ -2045,7 +2059,7 @@ sanei_bjnp_find_devices (const char **conf_devices,
   memset( &scanner_sa, 0 ,sizeof( scanner_sa ) );
   PDBG (bjnp_dbg (LOG_INFO, "sanei_bjnp_find_devices, pixma backend version: %d.%d.%d\n",
 	PIXMA_VERSION_MAJOR, PIXMA_VERSION_MINOR, PIXMA_VERSION_BUILD));
-  bjnp_no_devices = 0;
+  bjnp_cleanup_devices ();
 
   for (i=0; i < BJNP_SOCK_MAX; i++)
     {
